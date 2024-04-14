43d9934f95c10e7cbe249f532ac3410b5df258db
Anna Medvedeva <anna@eunify.net>
2024-04-14 20:25:01 +0300

    Deleted Thread,Sleep() Separated Tests

--- HW18/Dropdown.cs
+++ HW18/Dropdown.cs
@@ -11,20 +11,51 @@ namespace HW18
     internal class Dropdown: Tests
     {
         [Test]
-        public void CheckDropdown()
+        public void CheckDropdownOptionCount()
         {
             driver.Navigate().GoToUrl("http://the-internet.herokuapp.com/dropdown");
             IWebElement dropdownList = driver.FindElement(By.XPath("//select[@id='dropdown']"));
             IList<IWebElement> options = dropdownList.FindElements(By.TagName("option"));
-            Assert.AreEqual(2, options.Count, "Unexpected number of options in the dropdown.");
-            Assert.AreEqual("Please select an option", options[0].Text, "Incorrect text for the first option.");
-            Assert.AreEqual("Option 1", options[1].Text, "Incorrect text for the second option.");
+            Assert.AreEqual(3, options.Count, "Unexpected number of options in the dropdown.");
+        }
+
+        [Test]
+        public void CheckFirstOptionText()
+        {
+            driver.Navigate().GoToUrl("http://the-internet.herokuapp.com/dropdown");
+            IWebElement dropdownList = driver.FindElement(By.XPath("//select[@id='dropdown']"));
+            IWebElement firstOption = dropdownList.FindElement(By.XPath(".//option[1]"));
+            Assert.AreEqual("Please select an option", firstOption.Text, "Incorrect text for the first option.");
+        }
+
+        [Test]
+        public void CheckSecondOptionText()
+        {
+            driver.Navigate().GoToUrl("http://the-internet.herokuapp.com/dropdown");
+            IWebElement dropdownList = driver.FindElement(By.XPath("//select[@id='dropdown']"));
+            IWebElement secondOption = dropdownList.FindElement(By.XPath(".//option[2]"));
+            Assert.AreEqual("Option 1", secondOption.Text, "Incorrect text for the second option.");
+        }
+
+        [Test]
+        public void SelectSecondOption()
+        {
+            driver.Navigate().GoToUrl("http://the-internet.herokuapp.com/dropdown");
+            IWebElement dropdownList = driver.FindElement(By.XPath("//select[@id='dropdown']"));
+            IList<IWebElement> options = dropdownList.FindElements(By.TagName("option"));
             options[1].Click();
             Assert.IsTrue(options[1].Selected, "Option 1 should be selected after clicking.");
+        }
+
+        [Test]
+        public void SelectFirstOption()
+        {
+            driver.Navigate().GoToUrl("http://the-internet.herokuapp.com/dropdown");
+            IWebElement dropdownList = driver.FindElement(By.XPath("//select[@id='dropdown']"));
+            IList<IWebElement> options = dropdownList.FindElements(By.TagName("option"));
             options[0].Click();
             Assert.IsTrue(options[0].Selected, "Please select an option should be selected after clicking.");
-
-
         }
+
     }
 }
--- HW18/Inputs.cs
+++ HW18/Inputs.cs
@@ -10,19 +10,42 @@ namespace HW18
     internal class Inputs : Tests
     {
         [Test]
-        public void CheckInputs()
+        public void CheckNumberInputs()
         {
             driver.Navigate().GoToUrl("http://the-internet.herokuapp.com/inputs");
             IWebElement input = driver.FindElement(By.TagName("input"));
             input.SendKeys("12345");
             Assert.AreEqual("12345", input.GetAttribute("value"), "Incorrect numeric value entered.");
-            input.Clear();
+            
+        }
+
+        [Test]
+        public void CheckLettersInputs()
+        {
+            driver.Navigate().GoToUrl("http://the-internet.herokuapp.com/inputs");
+            IWebElement input = driver.FindElement(By.TagName("input"));
             input.SendKeys("abcde");
             Assert.IsTrue(string.IsNullOrEmpty(input.GetAttribute("value")), "Input field should be empty.");
+            
+        }
+
+        [Test]
+        public void CheckArrowUpInputs()
+        {
+            driver.Navigate().GoToUrl("http://the-internet.herokuapp.com/inputs");
+            IWebElement input = driver.FindElement(By.TagName("input"));
             input.SendKeys(Keys.ArrowUp);
             Assert.AreEqual("1", input.GetAttribute("value"), "Incorrect value after pressing ARROW_UP.");
+           
+        }
+
+        [Test]
+        public void CheckArrowDownInputs()
+        {
+            driver.Navigate().GoToUrl("http://the-internet.herokuapp.com/inputs");
+            IWebElement input = driver.FindElement(By.TagName("input"));           
             input.SendKeys(Keys.ArrowDown);
-            Assert.AreEqual("0", input.GetAttribute("value"), "Incorrect value after pressing ARROW_DOWN.");
+            Assert.AreEqual("-1", input.GetAttribute("value"), "Incorrect value after pressing ARROW_DOWN.");
         }
-        }
+    }
     }
